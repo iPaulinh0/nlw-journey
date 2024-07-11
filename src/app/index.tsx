12c4632898b10a45d6577a26ff0044c43cb64e4a
“iPaulinh0” <pauloaraujo2607@gmail.com>
2024-07-11 04:40:21 -0300

    add: conditional rendering by steps

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,25 @@
 import { colors } from '@/styles/colors';
 import { View, Text, Image } from 'react-native';
 import { Input } from '@/components/input';
-import { MapPin, Calendar as IconCalendar, Settings2 } from 'lucide-react-native'
+import { MapPin, Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight } from 'lucide-react-native'
 import { Button } from '@/components/button';
+import { useState } from 'react';
+
+enum StepForm {
+  TRIP_DETAILS = 1,
+  ADD_EMAIL = 2,
+}
 
 export default function Index() {
+
+  const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILS)
+
+  function handleNextStepForm() {
+    if(stepForm === StepForm.TRIP_DETAILS) {
+      return setStepForm(StepForm.ADD_EMAIL)
+    }
+  }
+
   return(
     <View className="flex-1 items-center justify-center px-5">
       <Image  source={require("@/assets/logo.png")} className='h-8' resizeMode='contain' />
@@ -15,22 +30,47 @@ export default function Index() {
 
         <Input>
           <MapPin color={colors.zinc[400]}/>
-          <Input.Field placeholder='Para onde?' />
+          <Input.Field placeholder='Para onde?' editable={stepForm === StepForm.TRIP_DETAILS} />
         </Input>
 
         <Input>
           <IconCalendar color={colors.zinc[400]}/>
-          <Input.Field placeholder='Para onde?' />
+          <Input.Field placeholder='Quando?' editable={stepForm === StepForm.TRIP_DETAILS}/>
         </Input>
 
-        <View className='border-b py-3 border-zinc-800'>
-          <Button variant='secondary'>
-            <Button.Title>Alterar local/data</Button.Title>
-            <Settings2 color={colors.zinc[200]} size={20}/>
-          </Button>
-        </View>
+        {stepForm === StepForm.ADD_EMAIL && 
+          <>
+            <View className='border-b py-3 border-zinc-800'>
+              <Button variant='secondary' onPress={() => setStepForm(StepForm.TRIP_DETAILS)}>
+                <Button.Title>Alterar local/data</Button.Title>
+                <Settings2 color={colors.zinc[200]} size={20}/>
+              </Button>
+            </View>
 
+            <Input>
+              <UserRoundPlus color={colors.zinc[400]}/>
+              <Input.Field placeholder='Quem estará na viagem?' />
+            </Input>
+          </>
+        }
+
+
+        <Button onPress={handleNextStepForm}>
+            <Button.Title>
+              {
+                stepForm === StepForm.TRIP_DETAILS 
+                ? "Continuar"
+                : "Confirmar Viagem"
+              }
+            </Button.Title>
+            <ArrowRight color={colors.lime[950]} size={20}/>
+        </Button>
       </View>
+
+    <Text className='text-zinc-500 font-regular text-center text-base'>
+      Ao planejar sua viagem pela plann.er, você automaticamente concorda com nossos <Text className='text-zinc-300 underline'>termos de uso e políticas de privavcidade</Text>.
+    </Text>
+
     </View>
   )
 }
